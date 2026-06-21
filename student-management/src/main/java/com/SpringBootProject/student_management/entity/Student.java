0e061385221f723e354d3e1b1bc36ff4d8ef0cc9
dhavalbathe <dhavalbathe@gmail.com>
2026-06-21 19:14:54 +0530

    feat: add student entity with jpa mapping and constraints

---
+++ student-management/src/main/java/com/SpringBootProject/student_management/entity/Student.java
@@ -0,0 +1,32 @@
+package com.SpringBootProject.student_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "students",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+}
