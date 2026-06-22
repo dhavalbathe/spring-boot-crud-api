7b44cc23fe7ce94e2b39408e94263ec182ecc4d0
dhavalbathe <dhavalbathe@gmail.com>
2026-06-22 14:12:07 +0530

    feat: implement create student service with dto mapping builder function

--- student-management/src/main/java/com/SpringBootProject/student_management/service/StudentServiceImpl.java
+++ student-management/src/main/java/com/SpringBootProject/student_management/service/StudentServiceImpl.java
@@ -1,8 +1,43 @@
 package com.SpringBootProject.student_management.service;
 
+import com.SpringBootProject.student_management.dto.StudentResponseDto;
+import com.SpringBootProject.student_management.entity.Student;
+import com.SpringBootProject.student_management.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class StudentServiceImpl extends StudentService {
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+    @Override
+    public StudentResponseDto createStudent(Student student) {
+        Student createdStudent = studentRepository.save(student);
+        StudentResponseDto dto = StudentResponseDto.builder()
+                .id(student.getId())
+                .name(student.getName())
+                .email(student.getEmail())
+                .age(student.getAge()).build();
+
+        return dto;
+    }
+
+    @Override
+    public StudentResponseDto getStudentById(Long id){
+        return null;
+    }
+
+    public List<StudentResponseDto> getAllStudents(){
+        return null;
+    }
+    public StudentResponseDto updateStudent(Long id, Student student){
+        return null;
+    }
+    public void deleteStudent(Long id){
+        return;
+    }
 
 }
